c3a7623231a167504e36b03ff60674c381eeb56c
jhonnjc15 <jhonatanjcca@gmail.com>
2021-02-03 23:11:27 +0000

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+/**
+ * main- Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 0
+ */
+int main(void)
+	{
+	putchar('a');
+	putchar('n');
+	putchar('d');
+	putchar(32);
+	putchar('t');
+	putchar('h');
+	putchar('a');
+	putchar('t');
+	putchar(32);
+	putchar('p');
+	putchar('i');
+	putchar('e');
+	putchar('c');
+	putchar('e');
+	putchar(32);
+	putchar('o');
+	putchar('f');
+	putchar(32);
+	putchar('a');
+	putchar('r');
+	putchar('t');
+	putchar(32);
+	putchar('i');
+	putchar('s');
+	putchar(32);
+	putchar('u');
+	putchar('s');
+	putchar('e');
+	putchar('f');
+	putchar('u');
+	putchar('l');
+	putchar('\"');
+	putchar(32);
+	putchar('-');
+	putchar(32);
+	putchar('D');
+	putchar('o');
+	putchar('r');
+	putchar('a');
+	putchar(32);
+	putchar('K');
+	putchar('o');
+	putchar('r');
+	putchar('p');
+	putchar('a');
+	putchar('r');
+	putchar(',');
+	putchar(32);
+	putchar('2');
+	putchar('0');
+	putchar('1');
+	putchar('5');
+	putchar('-');
+	putchar('1');
+	putchar('0');
+	putchar('-');
+	putchar('1');
+	putchar('9');
+	putchar('\n');
+	return (1);
+}
