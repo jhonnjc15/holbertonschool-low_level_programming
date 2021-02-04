a552bd9a5e782326e446dc8624b1a7f805a704c9
jhonnjc15 <jhonatanjcca@gmail.com>
2021-02-04 00:23:09 +0000

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 /**
  * main- Short description, single line
- * @void: Description of parameter x
+ * @filedes: Description of parameter x
+ * @buf: Description of parameter x
+ * @nbyte: Description of parameter x
 (* a blank line
  * Description: Longer description of the function)?
 (* section header: Section description)*
  * Return: 1
  */
-int main(void)
+int write(int filedes, const char *buf, unsigned int nbyte);
+int main(int argc, char **argv)
 	{
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
