09a1b63c52106b5d0306552930c4c51750e531a2
jhonnjc15 <jhonatanjcca@gmail.com>
2021-02-04 21:59:09 +0000

    Write a program that prints all single digit numbers of base 10 starting from 0, without printf and puts.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 0
+ */
+
+int main(void)
+	{
+	int num;
+
+	for (num = 0 ; num <= 9 ; num++)
+		{
+		putchar(num + '0');
+	}
+	putchar ('\n');
+	return (0);
+}
