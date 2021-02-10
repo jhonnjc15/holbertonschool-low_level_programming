3e904a5162e84c0e9a2e1735d64bcc32fcb3640e
jhonnjc15 <jhonatanjcca@gmail.com>
2021-02-10 02:45:15 +0000

    Fibonacci sequence whose values do not exceed 4,000,000, write a program that finds and prints the sum of the even-valued terms, followed by a new line.

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * @void : Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long num1, num2, suma, suma_par;
+
+	num1 = 1;
+	num2 = 2;
+	suma_par = 0;
+	suma = num1 + num2;
+	while (suma < 4000000)
+		{
+		num1 = num2;
+		num2 = suma;
+		suma = num1 + num2;
+		if (suma % 2 == 0)
+			suma_par = suma_par + suma;
+	}
+	printf("%lu\n", suma_par);
+	return (0);
+}
