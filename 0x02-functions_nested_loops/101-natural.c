d218f4050f8779573ddb987a59ded3997bc75c54
jhonnjc15 <jhonatanjcca@gmail.com>
2021-02-10 01:48:44 +0000

    Write a program that computes and prints the sum of all the multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * @void : Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 0
+ */
+
+void main(void)
+	{
+	int n, suma;
+
+	suma = 0;
+	for (n = 1 ; n < 1024 ; n++)
+		if (n % 3 == 0 || n % 5 == 0)
+			{
+			suma = suma + n;
+		}
+	printf("%d\n", suma);
+}
