fd04bc14cf957d950fed23092135d23477fd6be4
jhonnjc15 <jhonatanjcca@gmail.com>
2021-02-19 01:48:44 +0000

    Write a function that encodes a string into 1337,  known as "eleet"

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,4 @@
 #include "holberton.h"
-/**
-* _strlen- print the alphabet
-* @s : A pointer to get the length of the string
-*
-* Return: the new value of the input parameter
-* On error, -1 is returned, and errno is set appropriately.
-*/
-
-int _strlen(char *s)
-	{
-	int contador;
-
-	contador = 0;
-	while (s[contador] != '\0')
-	{
-		contador++;
-	}
-	return (contador);
-}
-
 
 /**
 * leet - function that encodes a string into 1337.
@@ -30,24 +10,24 @@ int _strlen(char *s)
 
 char *leet(char *s)
 	{
-	int length1;
-	int length2;
+	int length;
 	int i1;
 	int i2;
-	char vocales[5] = "AEOTL";
-	char  numeros[5] = "43071";
+	char vocales[5] = {'A', 'E', 'O', 'T', 'L'};
+	char numeros[5] = {'4', '3', '0', '7', '1'};
 
-	length1 = _strlen(s);
-	length2 = _strlen(vocales);
-	for (i1 = 0 ; i1 < length1 ; i1++)
+	length = 5;
+	i1 = 0;
+	while (s[i1])
 	{
-		for (i2 = 0 ; i2 < length2 ; i2++)
+		for (i2 = 0 ; i2 < length ; i2++)
 		{
 			if (s[i1] == vocales[i2] || s[i1] == vocales[i2] + 32)
 			{
 				s[i1] = numeros[i2];
 			}
 		}
+		i1++;
 	}
 	return (s);
 }
