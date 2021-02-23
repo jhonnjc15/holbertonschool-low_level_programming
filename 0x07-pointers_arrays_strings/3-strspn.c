4f98ebad0e3cf1aa6dc3f2d286d3abff6e8154e5
jhonnjc15 <jhonatanjcca@gmail.com>
2021-02-23 01:46:11 +0000

    Write a function that gets the length of a prefix substring.

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*_strspn - fill the first n bytes of the memory area with a constant b
+*@s: A char pointer
+*@accept: A char pointer
+*Return: the modify pointer dest
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int contador;
+
+	i = 0;
+	j = 0;
+	contador = 0;
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				contador++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+			return (contador);
+	}
+	return (contador);
+}
