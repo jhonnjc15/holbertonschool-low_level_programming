f53972f9c2186acac431f692be25af681cb93f7f
jhonnjc15 <jhonatanjcca@gmail.com>
2021-02-24 23:04:06 +0000

    Write a function that returns the natural square root of a number.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+* raiz - Funcion to calculate the square root
+*@n: The number
+*@i: The number to change while compare with the real number
+*Return: 1 if the number is prime, 0 if not
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+int raiz(int n, int i)
+{
+	if (i > 1)
+	{
+		if (i * i == n)
+		{
+			return (i);
+		}
+		else
+		{
+			return (raiz(n, i - 1));
+		}
+	}
+	else
+		return (-1);
+}
+
+/**
+* _sqrt_recursion - Funcion to calculate the square root of a number
+*@n: The number
+*Return: the square root
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n > 1)
+	{
+		return (raiz(n, n - 1));
+	}
+	else
+		return (-1);
+}
