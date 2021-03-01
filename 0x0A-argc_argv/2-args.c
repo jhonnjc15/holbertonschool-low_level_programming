c00a1aee8aa1bfda7d615f759a1108e8c8eedcac
jhonnjc15 <jhonatanjcca@gmail.com>
2021-03-01 01:56:05 +0000

    Write a program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - print the name of the executable
+*@argc:The number of command line arguments
+*@argv: An array containing the program command line arguments (size of argc)
+*Return: 0
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc ; i ++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
