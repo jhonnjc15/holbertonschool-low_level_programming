51fe6cd832d94e83ea1a107d16d228f5d9aa5b26
jhonnjc15 <jhonatanjcca@gmail.com>
2021-03-03 22:23:58 +0000

    Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+/**
+*longitud - Give us the length of a string
+*@s: A char pointer string.
+*Return: 1 if s is a palondrome, 0 if not.
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+unsigned int longitud(char *s)
+{
+	if (!(*s))
+		return (0);
+	return (1 + longitud(s + 1));
+}
+
 /**
 *_strdup - function to copy an string with malloc
 *@str:The string that will be copied
@@ -15,10 +29,10 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	new_string = malloc(sizeof(char) * strlen(str) + 1);
+	new_string = malloc(sizeof(char) * longitud(str) + 1);
 	if (new_string == NULL)
 		return (NULL);
-	for (i = 0; i <= strlen(str) ; i++)
+	for (i = 0; i <= longitud(str) ; i++)
 	{
 		new_string[i] = str[i];
 	}
