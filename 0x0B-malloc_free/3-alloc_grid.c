61889f008b815da4b4a4ed65ae4cf2bad90738ec
jhonnjc15 <jhonatanjcca@gmail.com>
2021-03-04 02:09:49 +0000

    Write a function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
-*longitud - Give us the length of a string
-*@s: A char pointer string.
-*Return: 1 if s is a palondrome, 0 if not.
-*On error, -1 is returned, and errno is set appropriately.
-*/
-
-int longitud(char *s)
-{
-	if (!(*s))
-		return (0);
-	return (1 + longitud(s + 1));
-}
-
 /**
 *alloc_grid - function to create a 2D array
 *@width:the number of columns
@@ -26,18 +11,23 @@ int longitud(char *s)
 int **alloc_grid(int width, int height)
 {
 	int **D_array;
-	int i, j;
+	int i, j, k;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	D_array = malloc(sizeof(int) * width);
+	D_array = malloc(sizeof(int *) * width);
 	if (D_array == NULL)
 		return (NULL);
 	for (i = 0; i < height ; i++)
 	{
 		D_array[i] = malloc(sizeof(int) * height);
 		if (D_array[i] == NULL)
+		{
+			for (k = 0; k <= i; k++)
+				free(*(D_array + k));
+			free(D_array);
 			return (NULL);
+		}
 		for (j = 0; j < width ; j++)
 		{
 			D_array[i][j] = 0;
