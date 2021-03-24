6885565ae3c37c38671840d4756db7fb846e0fec
jhonnjc15 <jhonatanjcca@gmail.com>
2021-03-24 19:30:26 +0000

    Write a function that frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+*free_listint2 - function to free the list
+*@head: A pointer to pointer to head of the list
+*Return: the number of nodes
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temporal1, *temporal2;
+
+	temporal2 = *head;
+	while (temporal2 != NULL)
+	{
+		temporal1 = temporal2->next;
+		free(temporal2);
+		temporal2 = temporal1;
+	}
+}
