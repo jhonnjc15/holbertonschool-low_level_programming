7b3891f323fd5b06122adbbc0955f02fc262f696
jhonnjc15 <jhonatanjcca@gmail.com>
2021-03-25 03:30:56 +0000

    Write a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,9 +18,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	unsigned int contador;
 
-	new_node = malloc(sizeof(listint_t));
 	if (head == NULL)
 		return (NULL);
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	if (idx == 0)
@@ -40,7 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		contador++;
 	}
 	if (temporal == NULL)
-			return (NULL);
+		return (NULL);
 	new_node->n = n;
 	new_node->next = temporal;
 	temp2->next = new_node;
