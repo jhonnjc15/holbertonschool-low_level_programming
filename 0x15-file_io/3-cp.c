e7557632f3873cb5b15ee47758aff72d96e31a1a
jhonnjc15 <jhonatanjcca@gmail.com>
2021-03-31 22:28:13 +0000

    Write a program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+/**
+* main-check the code for Holberton School students.
+* @argc: the number of all the parameter.
+* @argv: the parameters.
+* Return: Always 0.
+*/
+int main(int argc, char **argv)
+{
+	ssize_t open_text, open_text2, write_text, read_text;
+	ssize_t _close, _close2;
+	char *buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	open_text = open(argv[1], O_RDONLY);
+	if (open_text == -1)
+		dprintf(2, "Error: Can't read %s\n", argv[1]), exit(98);
+	open_text2 = open(argv[2], O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
+	if (open_text2 == -1)
+		dprintf(2, "Error: Can't write %s\n", argv[2]), exit(99);
+	else
+	{
+		while ((read_text = read(open_text, buf, 1024)) > 0)
+		{
+			write_text = write(open_text2, buf, read_text);
+			if (read_text != write_text)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		}
+		if (read_text == -1)
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	}
+	_close = close(open_text);
+	if (_close == -1)
+		dprintf(2, "Error: Can't close from file %d\n", (int)open_text), exit(99);
+	_close2 = close(open_text2);
+	if (_close2 == -1)
+		dprintf(2, "Error: Can't close from file %d\n", (int)open_text2), exit(99);
+	return (0);
+}
