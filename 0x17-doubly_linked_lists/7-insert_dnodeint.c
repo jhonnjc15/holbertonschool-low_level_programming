dda4d9ff64e8393e1aba7d0f47a66e7038ecc044
jhonnjc15 <jhonatanjcca@gmail.com>
2021-05-02 16:37:42 +0000

    Fixing errors

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,8 +19,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	/*add_dnodeint recibe como param la direc de la direc de memoria de la lista*/
 	if (idx == 0)
 	{	new_node = add_dnodeint(h, n);
+		*h = new_node;
 		return (new_node); /*Y retorna la direccion de memoria*/
 	}
+
 
 	while (tmp != NULL && tmp->next != NULL)
 	{
