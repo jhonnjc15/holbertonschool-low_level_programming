9e58dea77f43c4efa71ccfb624ca9f86aed89322
jhonnjc15 <jhonatanjcca@gmail.com>
2021-07-03 01:53:59 +0000

    Fixing the case when there is 1 square.

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,7 +8,7 @@ def island_perimeter(grid):
     Return: Returns the perimeter of the island described in grid
     """
     perimeter = 0
-    perimetro_x_1 = {0: 0, 1: 3, 2: 2, 3: 1, 4: 0}
+    perimetro_x_1 = {0: 4, 1: 3, 2: 2, 3: 1, 4: 0}
     for raw in range(len(grid)):
         for ind_col in range(len(grid[raw])):
             counter = 0
@@ -35,5 +35,6 @@ def island_perimeter(grid):
                         counter = counter + 1
                 except IndexError:
                     pass
-            perimeter = perimeter + perimetro_x_1[counter]
+
+                perimeter = perimeter + perimetro_x_1[counter]
     return perimeter
