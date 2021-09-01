40ad99ca268b2916a7a8a6b3e1adf2a4ec8b1c47
jhonnjc15 <jhonatanjcca@gmail.com>
2021-09-01 02:00:43 +0000

    Function that searches for a value in a sorted array of integers.

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,38 @@
+#include"search_algos.h"
+
+/**
+ * advanced_binary - Function that searches for a value in a
+ * sorted array of integers using a recursive Binary search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: return the first index where value is located
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int i;
+	int mid;
+	int left = 0;
+	int right = size - 1;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		mid = (left + right) / 2;
+		if (array[mid] == value && (mid == left || array[mid - 1] != value))
+			return (mid);
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid;
+	}
+	return (-1);
+}
