8a4ff480279a33daf5c104375923a57ff3f6ee2b
jhonnjc15 <jhonatanjcca@gmail.com>
2021-09-01 02:12:07 +0000

    function that searches for a value in a sorted array of integers.

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,25 +1,24 @@
 #include"search_algos.h"
 
 /**
- * advanced_binary - Function that searches for a value in a
- * sorted array of integers using a recursive Binary search algorithm
+ * binary_search_re - Function that searches for a value in a
+ * sorted array of integers using the Binary search algorithm
  * @array: is a pointer to the first element of the array to search in
- * @size: is the number of elements in array
  * @value: is the value to search for
- * Return: return the first index where value is located
+ * @left: The first index of the sub-array
+ * @right: The last index of the sub-array
+ * Return: return the index where value is located
  */
 
-int advanced_binary(int *array, size_t size, int value)
+int binary_search_re(int *array, int left, int right, int value)
 {
 	int i;
 	int mid;
-	int left = 0;
-	int right = size - 1;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
 
-	while (left <= right)
+	if (right >= left)
 	{
 		printf("Searching in array: ");
 		for (i = left; i < right; i++)
@@ -29,10 +28,28 @@ int advanced_binary(int *array, size_t size, int value)
 		mid = (left + right) / 2;
 		if (array[mid] == value && (mid == left || array[mid - 1] != value))
 			return (mid);
-		if (array[mid] < value)
-			left = mid + 1;
-		else
-			right = mid;
+		if (array[mid] >= value)
+			return (binary_search_re(array, left, mid, value));
+		return (binary_search_re(array, mid + 1, right, value));
 	}
 	return (-1);
+}
+
+/**
+ * advanced_binary - Function that searches for a value in a
+ * sorted array of integers
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: return the index of the first value in the array
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int left = 0;
+	int right = size - 1;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	return (binary_search_re(array, left, right, value));
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,6 +9,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-int advanced_binary_recursive(int *array, size_t size, int value);
+int advanced_binary_prueba(int *array, size_t size, int value);
 
 #endif
